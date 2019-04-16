dd338a2faac3f48385ce0a201a35422fa3c53134
ollylinux <olly.mitchell@gmail.com>
2019-04-16 10:27:57 +0100

    expose

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ RUN apt-get update -y
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
+
+EXPOSE 2418
 ENTRYPOINT ["python"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -2,8 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def advice():
+    return "Don't cycle in between two red buses!"
 
+print('damn')
 if __name__ == 'main':
+    print('ayo')
     app.run(debug=True, host='0.0.0.0')
+
+
+app.run(debug=True, host='0.0.0.0', port=2418)
