c4c79cd30995dc898ff1d5fd86193103ec1ab6e8
windowsolly <olly.mitchell@gmail.com>
2019-04-16 12:45:09 +0100

    smiley#

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def advice():
-    return "Don't cycle in between two red buses, and remember to charge your lights."
+    return "Don't cycle in between two red buses, and remember to charge your lights.\n\n:-) heyyy"
 
 print('damn')
 if __name__ == 'main':
